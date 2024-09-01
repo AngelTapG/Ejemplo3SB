5d22d9dd3f5f03304c2a35a2f7681e68b474c58d
Ángel T <sadblack018@gmail.com>
2024-08-31 21:30:45 -0600

    Add files via upload

---
+++ fibonacci-sequence.py
@@ -0,0 +1,19 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[i-1] + fib[i-2])
+    
+    return fib
+
+# Ejemplo de uso
+n = 10
+resultado = fibonacci(n)
+print(f"Los primeros {n} números de la secuencia de Fibonacci son:")
+print(resultado)
